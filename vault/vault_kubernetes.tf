fad80d6320ab075b585abfa249758c70dec430a8
raman-antanevich <raman.antanevich@productmadness.com>
2021-09-15 11:00:28 +0300

    comment out kubernetes_roles

--- vault/vault_kubernetes.tf
+++ vault/vault_kubernetes.tf
@@ -1,21 +1,21 @@
-variable "kubernetes_roles" {
-  type = map(any)
-  default = {
-    madness = {
-      backend    = "minikube"
-      accounts   = ["golang", "python"]
-      namespaces = ["dev-01", "stg-01", "uat-01"]
-      policies   = ["secret"]
-    }
-  }
-}
+# variable "kubernetes_roles" {
+#   type = map(any)
+#   default = {
+#     madness = {
+#       backend    = "minikube"
+#       accounts   = ["golang", "python"]
+#       namespaces = ["dev-01", "stg-01", "uat-01"]
+#       policies   = ["secret"]
+#     }
+#   }
+# }
 
-resource "vault_kubernetes_auth_backend_role" "kubernetes" {
-  for_each = var.kubernetes_roles
+# resource "vault_kubernetes_auth_backend_role" "kubernetes" {
+#   for_each = var.kubernetes_roles
 
-  backend                          = each.value.backend
-  role_name                        = each.key
-  bound_service_account_names      = each.value.accounts
-  bound_service_account_namespaces = each.value.namespaces
-  token_policies                   = each.value.policies
-}
+#   backend                          = each.value.backend
+#   role_name                        = each.key
+#   bound_service_account_names      = each.value.accounts
+#   bound_service_account_namespaces = each.value.namespaces
+#   token_policies                   = each.value.policies
+# }
